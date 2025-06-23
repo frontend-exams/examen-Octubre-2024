9e05d83eda55b3ba3660377fc431bfc21fd72dc8
Marcos5399 <147440358+Marcos5399@users.noreply.github.com>
2025-06-23 16:57:14 +0200

    Initialize solution

---
+++ DeliverUS-Frontend-Owner/src/api/RestaurantEndpoints.js
@@ -0,0 +1,32 @@
+import { get, post, put, destroy } from './helpers/ApiRequestsHelper'
+
+function getAll () {
+  return get('users/myrestaurants')
+}
+
+function getDetail (id) {
+  return get(`restaurants/${id}`)
+}
+
+function getRestaurantCategories () {
+  return get('restaurantCategories')
+}
+
+function create (data) {
+  return post('restaurants', data)
+}
+
+function update (id, data) {
+  return put(`restaurants/${id}`, data)
+}
+
+function remove (id) {
+  return destroy(`restaurants/${id}`)
+}
+
+// TODO: [Octubre 2024]
+function getOrdersFromRestaurant (id) {
+  return get(`restaurants/${id}/orders`)
+}
+
+export { getAll, getDetail, getRestaurantCategories, create, update, remove, getOrdersFromRestaurant }
